e06560995693feb63a77ae193068d07f01ad4b27
Aleksandr Egert <9213855+kreigan@users.noreply.github.com>
2026-09-02 16:19:29 -0400

    Add configuration for starship

---
+++ home/dot_config/rc.d/95-starship.sh
@@ -0,0 +1,9 @@
+{{ template "managed_by_chezmoi.tmpl" }}
+
+if command -v starship >/dev/null 2>&1; then
+  if [ -n "$ZSH_VERSION" ]; then
+    eval "$(starship init zsh)"
+  elif [ -n "$BASH_VERSION" ]; then
+    eval "$(starship init bash)"
+  fi
+fi
